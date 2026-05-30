7f97e5bc9adc60fc0786f8cef1e76ee31eeea2b2
帅帅的小站 <shuaidaoya@gmail.com>
2026-05-31 04:51:00 +0800

    更新 _worker.js

--- _worker.js
+++ _worker.js
@@ -405,12 +405,7 @@ export default {
 						}
 					}
 					return fetch(Pages静态页面 + '/login');
- 				} else if (访问路径 === 'admin/security' || 访问路径.startsWith('admin/security/')) {
-					const cookies = request.headers.get('Cookie') || '';
-					const authCookie = cookies.split(';').find(c => c.trim().startsWith('auth='))?.split('=')[1];
-					if (!authCookie || authCookie !== await MD5MD5(UA + 加密秘钥 + 管理员密码)) return new Response('重定向中...', { status: 302, headers: { 'Location': '/login' } });
-					return fetch(Pages静态页面 + '/admin/security' + url.search);
-				} else if (访问路径 === 'admin' || 访问路径.startsWith('admin/')) {//验证cookie后响应管理页面
+ 				} else if (访问路径 === 'admin' || 访问路径.startsWith('admin/')) {//验证cookie后响应管理页面
 					const cookies = request.headers.get('Cookie') || '';
 					const authCookie = cookies.split(';').find(c => c.trim().startsWith('auth='))?.split('=')[1];
 					// 没有cookie或cookie错误，跳转到/login页面
